c6439e4145d6c9f452b40d5da011ad38fbb5eeaf
xadrezo <58777516+xadrezo@users.noreply.github.com>
2025-05-05 02:18:34 +0200

    Slight code refactoring for ads in OK

---
+++ m/xadrezo/ok/ads.js
@@ -0,0 +1,13 @@
+function loadAds() {
+	/*
+	var adIds = 2
+
+	var randomSel = (Math.floor(Math.random() * adIds)).toString()
+	document.querySelector("#ad").src = "./limi/ad-" + randomSel + ".png"
+
+	var randomSel1 = (Math.floor(Math.random() * adIds)).toString()
+	document.querySelector("#ad1").src = "./limi/ad-" + randomSel1 + ".png"
+	*/
+}
+
+loadAds()
